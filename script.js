93774b223562d12099fd7015791b3f913e0bb087
Malsha Gimhani <dev.malsha93@gmail.com>
2023-03-10 16:04:53 +0530

    Create Buttons

---
+++ script.js
@@ -0,0 +1,64 @@
+function clickedButton1() {
+  const clickedNumberIs = document.getElementById("num1").value;
+  console.log(clickedNumberIs);
+}
+
+function clickedButton2() {
+  const clickedNumberIs = document.getElementById("num2").value;
+  console.log(clickedNumberIs);
+}
+
+function clickedButton3() {
+  const clickedNumberIs = document.getElementById("num3").value;
+  console.log(clickedNumberIs);
+}
+
+function clickedButton4() {
+  const clickedNumberIs = document.getElementById("num4").value;
+  console.log(clickedNumberIs);
+}
+
+function clickedButton5() {
+  const clickedNumberIs = document.getElementById("num5").value;
+  console.log(clickedNumberIs);
+}
+
+function clickedButton6() {
+  const clickedNumberIs = document.getElementById("num6").value;
+  console.log(clickedNumberIs);
+}
+
+function clickedButton7() {
+  const clickedNumberIs = document.getElementById("num7").value;
+  console.log(clickedNumberIs);
+}
+
+function clickedButton8() {
+  const clickedNumberIs = document.getElementById("num8").value;
+  console.log(clickedNumberIs);
+}
+
+function clickedButton9() {
+  const clickedNumberIs = document.getElementById("num9").value;
+  console.log(clickedNumberIs);
+}
+
+function clickedButton0() {
+  const clickedNumberIs = document.getElementById("num0").value;
+  console.log(clickedNumberIs);
+}
+
+function clickedButtonAdd() {
+  const clickedNumberIsAdd = document.getElementById("add").value;
+}
+
+function clickedButtonSub() {
+  const clickedNumberIs = document.getElementById("sub").value;
+}
+function clickedButtonMulti() {
+  const clickedNumberIs = document.getElementById("multi").value;
+}
+function clickedButtonDivi() {
+  const clickedNumberIs = document.getElementById("divi").value;
+}
+function clickedButtonEqual() {}
