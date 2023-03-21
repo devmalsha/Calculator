cc45af5058bd1583e384c93d9f2a84f90989ca18
Malsha Gimhani <dev.malsha93@gmail.com>
2023-03-21 21:12:50 +0530

    Set clicked buttons to displaying as a row in display div

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const clicked = (btnId) => {
-  document.getElementById("display").innerHTML =
-    document.getElementById(btnId).id;
-};
+// const clicked = (btnId) => {
+//   // document.getElementById("display").innerHTML =
+//   //   document.getElementById(btnId).id;
+//   let num1 = document.getElementById(btnId).innerText;
+//   document.getElementById("display").innerHTML = num1;
+// };
 
-let displayedNumber = document.getElementById("display").innerText;
-document.getElementById("display").innerHTML = displayedNumber;
-console.log(displayedNumber);
+let ele1 = "";
+let ele2 = "";
+const clicked = (idIs) => {
+  ele2 = document.getElementById(idIs).id;
+  let display = document.getElementById("display").innerHTML;
+  if (display == 0) {
+    document.getElementById("display").innerHTML = ele2;
+  } else if (display !== 0) {
+    ele2 = display * 10 + ele2 * 1;
+    document.getElementById("display").innerHTML = ele2;
+  }
+};
