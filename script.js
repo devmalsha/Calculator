7565048e6795c8f55b5a8aa538e1d5b7fad1fa69
Malsha Gimhani <dev.malsha93@gmail.com>
2023-03-22 14:57:51 +0530

    Stuck + operator

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-// const clicked = (btnId) => {
-//   // document.getElementById("display").innerHTML =
-//   //   document.getElementById(btnId).id;
-//   let num1 = document.getElementById(btnId).innerText;
-//   document.getElementById("display").innerHTML = num1;
-// };
-
-let ele1 = "";
-let ele2 = "";
+let firstNum = "";
+let secNum = "";
+let currentValue = "";
+let operator = "";
 const clicked = (idIs) => {
-  ele2 = document.getElementById(idIs).id;
+  secNum = document.getElementById(idIs).id;
   let display = document.getElementById("display").innerHTML;
-  if (display == 0) {
-    document.getElementById("display").innerHTML = ele2;
-  } else if (display !== 0) {
-    ele2 = display * 10 + ele2 * 1;
-    document.getElementById("display").innerHTML = ele2;
+  if (operator == "+") {
+  } else {
+    if (display == 0) {
+      document.getElementById("display").innerHTML = secNum;
+    } else if (display !== 0) {
+      secNum = display * 10 + secNum * 1;
+      document.getElementById("display").innerHTML = secNum;
+    }
   }
+};
+
+const doAddition = (sumId) => {
+  operator = document.getElementById(sumId).innerHTML;
+  let ele1 = document.getElementById("display").innerHTML;
+
+  // equal(ele3);
+};
+
+const doSubstraction = (subId) => {
+  operator = document.getElementById(subId).innerHTML;
+  let ele1 = document.getElementById("display").innerHTML;
+
+  // equal(ele3);
+};
+
+const equal = (result) => {
+  document.getElementById("display").innerHTML = result;
 };
