4a13c6e5b458c83edd8075ad53a549185d82b362
Malsha Gimhani <dev.malsha93@gmail.com>
2023-03-23 18:19:49 +0530

    display any digit value after the operator

--- script.js
+++ script.js
@@ -6,11 +6,10 @@ let secDisplay;
 const clicked = (idIs) => {
   secNum = document.getElementById(idIs).innerHTML;
   let display = document.getElementById("display").innerHTML;
-  //+
+
   if (operator) {
     if (secDisplay) {
       secDisplay = secDisplay * 10 + secNum * 1;
-      console.log(secDisplay);
       document.getElementById("display").innerHTML = secDisplay;
     } else {
       document.getElementById("display").innerHTML = secNum;
@@ -21,7 +20,7 @@ const clicked = (idIs) => {
       document.getElementById("display").innerHTML = secNum;
     } else if (display !== 0) {
       secNum = display * 10 + secNum * 1;
-      document.getElementById("display").innerHTML = secNum; //secnum - 34
+      document.getElementById("display").innerHTML = secNum;
     }
   }
 };
@@ -29,6 +28,7 @@ const clicked = (idIs) => {
 const doAddition = (sumId) => {
   operator = document.getElementById(sumId).innerHTML;
   let ele1 = document.getElementById("display").innerHTML;
+  secDisplay = "";
 
   // equal(ele3);
 };
