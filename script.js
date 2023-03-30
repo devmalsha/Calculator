17ace020d2df4d0c416156c99870975ae07129c0
Malsha Gimhani <dev.malsha93@gmail.com>
2023-03-30 19:01:16 +0530

    subtract 2 numbers. pop with - mark

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const clicked = (idIs) => {
       document.getElementById("display").innerHTML = secDisplay;
     } else {
       document.getElementById("display").innerHTML = secNum;
+
       secDisplay = secNum;
     }
   } else {
@@ -26,7 +27,6 @@ const clicked = (idIs) => {
     } else if (display !== 0) {
       secNum = display * 10 + secNum * 1;
       document.getElementById("display").innerHTML = secNum;
-      // afterOperator = document.getElementById("display").innerHTML;
     }
   }
 };
@@ -34,28 +34,16 @@ const clicked = (idIs) => {
 const addition = (sumId) => {
   operator = document.getElementById(sumId).innerHTML;
   beforeOperator = document.getElementById("display").innerHTML;
-  // if (!afterOperator) {
-  //   afterOperator = 0;
-  //   afterOperator = afterOperator * 1 + beforeOperator * 1;
-  //   console.log(
-  //     "After Operator :" +
-  //       afterOperator +
-  //       " & " +
-  //       "Before Operator :" +
-  //       beforeOperator
-  //   );
-  //   document.getElementById("display").innerHTML = afterOperator;
-  // } else {
-  //   afterOperator = afterOperator * 1 + beforeOperator * 1;
-  //   document.getElementById("display").innerHTML = afterOperator;
-  // }
   afterOperator = afterOperator * 1 + beforeOperator * 1;
   document.getElementById("display").innerHTML = afterOperator;
   secDisplay = "";
 };
 
-const doSubstrac = (subId) => {
+const subtraction = (subId) => {
   operator = document.getElementById(subId).innerHTML;
+  beforeOperator = document.getElementById("display").innerHTML;
+  afterOperator = beforeOperator * 1 - afterOperator * 1;
+  document.getElementById("display").innerHTML = afterOperator;
   secDisplay = "";
 };
 
